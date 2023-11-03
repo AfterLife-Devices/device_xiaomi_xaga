5853107b2d2bf7a1199be76a37eb9dec2dc01e5a
Master <mhmdeveloperofficial@gmail.com>
2023-11-03 04:49:21 +0000

    xaga: Add ViPER4Android FX support
    * Need to clone this:
    git clone https://github.com/swiitch-OFF-Lab/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: ryzenforce990 <ryzenforce990@gmail.com>

--- device.mk
+++ device.mk
@@ -491,12 +491,15 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
-    
+
 # VNDK
 PRODUCT_PACKAGES += \
     libutils-v32
 
-# Wi-Fi
+# Viper
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
+# WiFi
 PRODUCT_PACKAGES += \
     wpa_supplicant \
     hostapd \
