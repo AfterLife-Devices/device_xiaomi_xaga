48e0f18f91318c76cc87a8c42ebd18db9690d99c
ChrisCatto <chriscatto08@gmail.com>
2023-11-24 19:59:16 +0200

    xaga: Drop our custom lights service
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- device.mk
+++ device.mk
@@ -220,10 +220,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.lights-service.xiaomi_mt6895
-
 # Linker
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
