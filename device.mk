c403ce45f5cb2ea2ccdc47221013d6fb21da2fff
ChrisCatto <chriscatto08@gmail.com>
2023-11-28 10:19:44 +0000

    xaga: Include `hardware/google`,`hardware/mediatek`.`hardware/xiaomi` soong namespaces

--- device.mk
+++ device.mk
@@ -444,7 +444,11 @@ BOARD_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/mediatek \
+    hardware/xiaomi \
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
